477d1b55d6c62670b398f79e57f404d2fa4fce6a
aeongt710 <unknowndonlion@gmail.com>
2022-10-20 08:37:08 +0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HCILabForm.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HCILabForm.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<UniForm> UniForms { get; set; }
+    }
+}
---
+++ Models/UniForm.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HCILabForm.Models
+{
+    public class UniForm
+    {
+        public int Id { get; set; } = 0;
+        [Required]
+        //[RegularExpression("([a-zA-Z]*)", ErrorMessage = "Invalid Format")]
+        public string Name { get; set; }
+        [Required]
+        //[RegularExpression("([a-zA-Z]*)", ErrorMessage = "Invalid Format")]
+        public string FatherName { get; set; }
+        [Required]
+        public string? City { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        public bool isForeign { get; set; } = false;
+        [Required]
+        public string Campus { get; set; }
+        [Required]
+        public string Program { get; set; }
+        public string? PreviousField { get; set; }
+        [Required]
+        public string PreviousGrade { get; set; }
+        [Required]
+        //[RegularExpression("([0-9]{13})",ErrorMessage ="Invalid Format")]
+        public string CNIC { get; set; }
+        [Required]
+        public string? Domicile { get; set; }
+        [Required]
+        public string? Country { get; set; }
+    }
+}
